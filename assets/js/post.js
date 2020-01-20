07951bf175808df7f05eb8b65193520c7a6a00d0
darrenplace <48530717+darrenplace@users.noreply.github.com>
2020-01-20 14:37:22 -0600

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -110,13 +110,13 @@
 	// ——————————————————————————————————————————————————
 
 	var heroBG = [
-		'../assets/img/heroBG/1.jpg',
-		'../assets/img/heroBG/2.jpg',
-		'../assets/img/heroBG/3.jpg',
-		'../assets/img/heroBG/4.jpg',
-		'../assets/img/heroBG/5.jpg',
-		'../assets/img/heroBG/6.jpg',
-		'../assets/img/heroBG/7.jpg'
+		'assets/img/heroBG/1.jpg',
+		'assets/img/heroBG/2.jpg',
+		'assets/img/heroBG/3.jpg',
+		'assets/img/heroBG/4.jpg',
+		'assets/img/heroBG/5.jpg',
+		'assets/img/heroBG/6.jpg',
+		'assets/img/heroBG/7.jpg'
 	];
 
 	var rand = heroBG[Math.floor(Math.random() * heroBG.length)];
