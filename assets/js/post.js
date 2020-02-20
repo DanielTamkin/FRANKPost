46dd3ba2fc49e998c2ced78404fd62ec83d96222
darrenplace <48530717+darrenplace@users.noreply.github.com>
2020-02-20 16:17:30 -0600

    clean repo

--- assets/js/post.js
+++ assets/js/post.js
@@ -44,13 +44,14 @@
 		delay: 3000,
 		transitionDuration: 1000,
 		images: [
+			"./assets/img/heroBG/0.jpg",
 			"./assets/img/heroBG/1.jpg",
 			"./assets/img/heroBG/2.jpg",
 			"./assets/img/heroBG/3.jpg",
 			"./assets/img/heroBG/4.jpg",
 			"./assets/img/heroBG/5.jpg",
 			"./assets/img/heroBG/6.jpg",
-			"./assets/img/heroBG/7.jpg",
+			"./assets/img/heroBG/7.jpg"
 		]
 	})
 
